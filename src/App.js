097981bed6cd789461c59f6e00161246af48e90d
John Mungcal <145724129+johnmungcal2@users.noreply.github.com>
2024-05-02 07:36:19 +0800

    Added button functionalities

--- src/App.js
+++ src/App.js
@@ -1,66 +1,99 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { useState } from "react";
 
-
-function CalcButton({label, buttonClassName = "CalcButton", onClick}) {
-
+function CalcButton({ label, buttonClassName = "CalcButton", onClick }) {
   return (
-    <button className= {buttonClassName} onClick={onClick}>
+    <button className={buttonClassName} onClick={onClick}>
       {label}
     </button>
   );
 }
 
-function CalcDisplay({display}) {
+function CalcDisplay({ display }) {
   return (
-    <div className="CalcDisplay">
-      {display}
-    </div>    
+    <div className="output-display">
+      <div className="previous-operand"></div>
+      <div className="current-operand">{display}</div>
+    </div>
   );
 }
 
-export default function App() {
+function App() {
+  const [disp, setDisp] = useState(0);
+  const [num1, setNum1] = useState(null);
+  const [num2, setNum2] = useState(null);
+  const [op, setOp] = useState(null);
 
-  const[disp, setDisp] = useState(0);
+  const clrClickHandler = () => {
+    setDisp(0);
+    setNum1(null);
+    setNum2(null);
+    setOp(null);
+  };
 
-  const clickHandler = (e) => {
-    e.preventDefault();
-    const value  = e.target.innerHTML;
+  const equalClickHandler = () => {
+    console.log('Num1 ' + num1 + ' | ' + 'Op ' + op + ' | ' + 'Num2 ' + num2);
+    if (op === "+") {
+      setDisp(parseInt(num1) + parseInt(num2));
+    } else if (op === "-") {
+      setDisp(parseInt(num1) - parseInt(num2));
+    } else if (op === "x") {
+      setDisp(parseInt(num1) * parseInt(num2));
+    } else if (op === "÷") {
+      setDisp(parseInt(num1) / parseInt(num2));
+    } else {
+      setDisp('Invalid Operation');
+    }
+  };
+
+  const numberClickHandler = (value) => {
+    if (op === null) {
+      if (num1 === null) {
+        setNum1(value);
+        setDisp(value);
+      } else {
+        setNum1(num1 + value);
+        setDisp(num1 + value);
+      }
+    } else {
+      if (num2 === null) {
+        setNum2(value);
+        setDisp(value);
+      } else {
+        setNum2(num2 + value);
+        setDisp(num2 + value);
+      }
+    }
+  };
+
+  const opClickHandler = (value) => {
+    setOp(value);
     setDisp(value);
-    //alert('You clicked: ' + value);
-  }
-
-  const numberClickHandler = (e) => {
-    e.preventDefault();
-    const value  = e.target.innerHTML;
-    setDisp(value);
-    //alert('You clicked: ' + value);
-  }
+  };
 
   return (
-    <div className="App" style={{backgroundColor: '#c4cbe1'}}>
-      <div className="CalcContainer">
-        <CalcDisplay display={disp} />
-        <div className="ButtonContainer">
-        <CalcButton label={"/"} onClick={clickHandler} buttonClassName="operationButton"/>
-          <CalcButton label={7} onClick={numberClickHandler}/>
-          <CalcButton label={8} onClick={numberClickHandler}/>
-          <CalcButton label={9} onClick={numberClickHandler}/>
-          <CalcButton label={"x"} onClick={clickHandler} buttonClassName="operationButton"/>
-          <CalcButton label={4} onClick={numberClickHandler}/>
-          <CalcButton label={5} onClick={numberClickHandler}/>
-          <CalcButton label={6} onClick={numberClickHandler}/>
-          <CalcButton label={"+"} onClick={clickHandler} buttonClassName="operationButton"/>
-          <CalcButton label={1} onClick={numberClickHandler}/>
-          <CalcButton label={2} onClick={numberClickHandler}/>
-          <CalcButton label={3} onClick={numberClickHandler}/>
-          <CalcButton label={"-"} onClick={clickHandler} buttonClassName="operationButton"/>
-          <CalcButton label={"C"} onClick={clickHandler}/>
-          <CalcButton label={0} onClick={numberClickHandler}/>
-          <CalcButton label={"="} onClick={clickHandler} buttonClassName="operationButton"/>
-        </div>
+    <div className="calculator-container">
+      <CalcDisplay display={disp} />
+      <div className="button-grid">
+        <CalcButton label={"÷"} buttonClassName="operation" onClick={() => opClickHandler("÷")} />
+        <CalcButton label={7} onClick={() => numberClickHandler("7")} />
+        <CalcButton label={8} onClick={() => numberClickHandler("8")} />
+        <CalcButton label={9} onClick={() => numberClickHandler("9")} />
+        <CalcButton label={"x"} buttonClassName="operation" onClick={() => opClickHandler("x")} />
+        <CalcButton label={4} onClick={() => numberClickHandler("4")} />
+        <CalcButton label={5} onClick={() => numberClickHandler("5")} />
+        <CalcButton label={6} onClick={() => numberClickHandler("6")} />
+        <CalcButton label={"+"} buttonClassName="operation" onClick={() => opClickHandler("+")} />
+        <CalcButton label={1} onClick={() => numberClickHandler("1")} />
+        <CalcButton label={2} onClick={() => numberClickHandler("2")} />
+        <CalcButton label={3} onClick={() => numberClickHandler("3")} />
+        <CalcButton label={"-"} buttonClassName="operation" onClick={() => opClickHandler("-")} />
+        <CalcButton label={"C"} buttonClassName="operation" onClick={clrClickHandler} />
+        <CalcButton label={0} onClick={() => numberClickHandler("0")} />
+        <CalcButton label={"="} buttonClassName="operation" onClick={equalClickHandler} />
       </div>
     </div>
   );
 }
+
+export default App;
